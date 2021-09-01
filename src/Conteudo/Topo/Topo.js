98cbd6ca7749ac641ad6261d4cec9eca9322f3e9
ligia-davila-bozzi <ligia@respondeai.com.br>
2021-09-01 14:10:36 -0300

    preparando review

--- src/Conteudo/Topo/Topo.js
+++
@@ -1,8 +0,0 @@
-export default function Topo() {
-    return (
-        <div className="topo">
-            <p className="logo-DrivenEats">DrivenEats</p>
-            <p className="frase-abaixo-logo">Sua comida em 6 minutos</p>
-        </div>
-    )
-}
